0a8960f063bb6eddcc8cef54f1ddbb39eadda0e0
silvestre18 <jim@MacBook-Air-de-JIM.local>
2015-08-05 22:26:41 -0500

    Understanding primitive types

---
+++ _1_DataTypes_and_Operators/_1_Primitive_Types/primitives.java
@@ -0,0 +1,45 @@
+/* Right now it is not important to understand how classes are created
+ The cornerstone of this file is to understand the different kinds of primitive types,
+ their features and how to use them */
+
+class primitives
+{
+  public static void main(String ...a)
+  {
+    // INTEGERS  ( 23, -456 , 0 )
+    // Java defines four types of integers
+    
+
+    byte a1;      // byte type has 8 bits per number
+    short a2;     // short type has 16 bits per number
+    int a3;       // int type has 32 bits per number
+    long a4;      // long type has 64 bits per number
+    
+    // FLOATING-POINTS  (23.4 , -34.56 , -123,4555)
+    // Java defines two types of floating-points
+    
+    
+    float a5;     // this saves a single-precision number or 32 bits    
+    double a6;    // this saves a double-precision number or 64 bits
+    
+    // CHARACTERS ('E', '`' , '5')
+    // Java uses the standard UNICODE to represent characters
+    // any character has 16 bits
+    
+    char a7;
+    
+    // BOOLEAN (true , false)
+    // There are only two values for boolean ones 
+    
+    boolean a8 ; // this is how a boolean variable is defined
+    
+    
+    
+    /* Right now we are not showing hot to initialize variables
+    the only aspect in this file is how to declare variable from
+    differente types */
+  
+  
+  }
+
+}
