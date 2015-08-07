51fdafe4cd2b7ca1a0ca4da7104b9c35d5e01805
silvestre18 <jim@MacBook-Air-de-JIM.local>
2015-08-07 01:30:04 -0500

    Understanding Statement Do While

---
+++ _2_Program_Control_Statements/_5_The_DoWhile_Statement/statementdowhile.java
@@ -0,0 +1,23 @@
+// This file will explain all the features of the statement do while
+class statementdowhile
+{
+  public static void main(String aa[])
+  {
+    /* A simple example of the use of do while */
+    
+    int a =5;
+    do
+    {
+      System.out.print(a +" ");
+      a--;
+      /* This will print 
+       5 4
+      */
+    
+    }while(a>3);      // the do while guarantee to execute the body at least one time
+    
+    // Do not forget the ; at the end of while
+  
+  }
+
+}
