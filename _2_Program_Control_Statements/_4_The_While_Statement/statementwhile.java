938f2e1bbe324d95ac50384096259c9ec17b52fa
silvestre18 <jim@MacBook-Air-de-JIM.local>
2015-08-07 01:25:18 -0500

    Understanding Statement While

---
+++ _2_Program_Control_Statements/_4_The_While_Statement/statementwhile.java
@@ -0,0 +1,26 @@
+/* This file will explain all the features of the while statement */
+class statementwhile
+{
+  public static void main(String aa[])
+  {
+    /* A simple example of the use of while */
+    
+    int a =5;
+    while(a >2)        // what there is inside braces must be false to stop executing the while
+    {
+      System.out.println(a+" ");
+      a--;
+    }
+    
+    /*
+     this while will print 5 4 3
+     because there will be a moment in which a stops being greater than 2
+     in that moment a>2 will be false and the whole while will stop */
+     
+  
+  
+  
+  }
+
+
+}
