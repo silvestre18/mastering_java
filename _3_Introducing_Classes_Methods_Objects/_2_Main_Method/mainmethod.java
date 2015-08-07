ecbed5addf89a642e1d2731f4c1ff59e5e0a6985
silvestre18 <jim@MacBook-Air-de-JIM.local>
2015-08-07 10:20:45 -0500

    Understanding main Method

---
+++ _3_Introducing_Classes_Methods_Objects/_2_Main_Method/mainmethod.java
@@ -0,0 +1,22 @@
+/* Finally it will be explained the main method */
+
+/* First of all , the main method always belong to a class , so this class
+   must have the name of the file , It is mandatory */
+
+class mainmethod          // this class has the name of the file
+{
+  // As it is seen , the main method is belongs to the class which has the same name as the file
+  
+  public static void main(String aa[]) // String aa[] is an array , it will be explained later
+  {
+  
+  // Every program in java via console is started in the main method
+  
+  
+  
+  
+  
+  
+  }
+
+}
