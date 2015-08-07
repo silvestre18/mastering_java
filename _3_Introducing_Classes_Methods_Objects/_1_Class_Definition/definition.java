08d9bb5b155745057ebda39d164fb180470e4e74
silvestre18 <jim@MacBook-Air-de-JIM.local>
2015-08-07 10:07:01 -0500

    Understanding Structure of a Class

---
+++ _3_Introducing_Classes_Methods_Objects/_1_Class_Definition/definition.java
@@ -0,0 +1,56 @@
+/* This file will explain the structure of a class */
+
+/* The first concept to be understood is the difference between a file.java and a file.class
+   the code is saved in a file.java , this file is compiled and creates a file.class which can be 
+   read by the JVM and run the program */
+   
+
+/* Now , this is a normal class 
+   
+   class nameoftheclass            // we have to start with the keyword class and a name 
+   {
+     // comments                   // comments can go anywhere , before of after the definition of the class
+     
+     int a1;                        // those are called objects field or instance variables
+     char b1;                       // those will be explained later
+     
+     int firstmethod(int a)         // this is a methods , this will be explained later
+     {                              // this is the body of a method
+     }
+     
+     nameoftheclass()               // this is a constructor of the class
+     {                              // this will be also explained later
+     }
+   
+   
+   
+   }
+
+*/
+
+
+// Now a real example
+class definition           // a class called definition
+{
+  // Those are comments , and as you can se int this an former files , those can be placed anywhere
+  
+  int a1;                    // a instance variable
+  
+  int firstmethod(int aa1)   // a method
+  {
+  }
+  
+  definition()             // a constructor (it must have the same name of the class)
+  {
+  
+  }
+  
+  /*****************NOTES*******************/
+  
+  // Variables , methods and constructors will be explained better in following files
+  
+  
+    
+  
+
+}
